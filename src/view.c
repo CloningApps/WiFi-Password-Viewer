46a6b702e6577a5158abbab41c25ac621a5fb81e
CloningApps <collabvm.akka@gmail.com>
2024-12-12 23:03:04 +0100

    Update and rename view.c to src/view.c

--- src/view.c
+++ src/view.c
@@ -2,14 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
-#include <unistd.h>
 #include <ctype.h>
-
-#define MAX_PATH 1024
-#define MAX_LINE 256
-
-int android_mode = 0;
-int no_root_mode = 0;
+#include "wifi_password.h"
 
 void trim(char *str) {
     char *end;
@@ -71,31 +65,3 @@ void process_directory(const char *dir_path) {
     closedir(dir);
 }
 
-int main(int argc, char *argv[]) {
-    const char *nm_dir = "/etc/NetworkManager/system-connections/";
-    const char *android_dir = "/data/misc/wifi/";
-
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "--android") == 0) {
-            android_mode = 1;
-        } else if (strcmp(argv[i], "--no-root") == 0) {
-            no_root_mode = 1;
-        }
-    }
-
-    if (!no_root_mode && geteuid() != 0) {
-        printf("This program requires root privileges. Please run with sudo or use --no-root.\n");
-        return 1;
-    }
-
-    printf("WiFi Passwords:\n");
-    printf("---\n");
-
-    if (android_mode) {
-        process_directory(android_dir);
-    } else {
-        process_directory(nm_dir);
-    }
-
-    return 0;
-}
